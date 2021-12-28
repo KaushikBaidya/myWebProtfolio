ee9d25279be1c091623030c931fff12c47adfb7a
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2021-12-28 23:06:13 +0600

    intro-left-completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,6 +8,21 @@ const Intro = () => {
         <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My Name is</h2>
           <h1 className="i-name">Kaushik Baidya</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web developer</div>
+              <div className="i-title-item">UI/Ux designer</div>
+              <div className="i-title-item">Content Creator</div>
+              <div className="i-title-item">Writer</div>
+              <div className="i-title-item">Photographer</div>
+            </div>
+          </div>
+          <p className="i-desc">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
         </div>
       </div>
       <div className="intro-right">Right</div>
