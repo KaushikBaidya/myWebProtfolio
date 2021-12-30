e630d94b1aac86912cc05eb57ac1c3506a44551f
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2021-12-30 15:40:46 +0600

    emailjs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from './components/about/About'
+import { Contact } from './components/contact/Contact'
 import Intro from './components/intro/Intro'
 import { Itemlist } from './components/itemlist/Itemlist'
 
@@ -8,6 +9,7 @@ function App() {
       <Intro />
       <About />
       <Itemlist />
+      <Contact />
     </div>
   )
 }
