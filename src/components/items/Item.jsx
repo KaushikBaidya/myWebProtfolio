7adb1b99242663d62531eab589a95bc919bf964a
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-01-02 23:51:28 +0600

    updated item.css

--- src/components/items/Item.jsx
+++ src/components/items/Item.jsx
@@ -4,9 +4,9 @@ export const Item = () => {
   return (
     <div className="item">
       <div className="item-brw">
-        <div className="item-circle"></div>
-        <div className="item-circle"></div>
-        <div className="item-circle"></div>
+        <div className="item-circle a"></div>
+        <div className="item-circle b"></div>
+        <div className="item-circle c"></div>
       </div>
     </div>
   )
